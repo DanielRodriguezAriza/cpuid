91c3cdaddf41b9e8f6e0af688544c7006b20a9b7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-11-16 20:15:06 +0100

    Created header for cpuid internal logic

---
+++ cpuid.h
@@ -0,0 +1,43 @@
+#ifndef CPUID_H
+#define CPUID_H
+
+#include <stdint.h>
+
+typedef union cpuid_t {
+	uint32_t a, b, c, d;
+	uint32_t eax, ebx, ecx, edx;
+	uint32_t regs[4];
+} cpuid_t;
+
+/* MSVC support */
+#if defined(_MSC_VER) && (defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64))
+	#define CPUID_VERSION_MSVC
+	#include <intrin.h>
+#endif
+
+/* GNUC GCC support */
+#if defined(__GNUC__) || defined(__MINGW32__) || defined(__MINGW64__)
+	#define CPUID_VERSION_GCC
+#endif
+
+/* LLVM Clang support */
+#if defined(__clang__) || defined(__llvm__)
+	#define CPUID_VERSION_LLVM
+#endif
+
+static inline void cpuid_call(uint32_t leaf, uint32_t subleaf, cpuid_t *registers)
+{
+#if defined(CPUID_VERSION_MSVC)
+	__cpuidex(regs->regs, leaf, subleaf);
+#elif defined(CPUID_VERSION_GCC) || defined(CPUID_VERSION_LLVM)
+	__asm__ volatile(
+		"cpuid"
+		: "=a"(regs->eax), "=b"(regs->ebx), "=c"(regs->ecx), "=d"(regs->edx)
+		: "a"(leaf), "c"(subleaf)
+	);
+#else
+	#error "Unsupported compiler: no CPUID intrinsic or inline assembly available or known to the implementation."
+#endif
+}
+
+#endif /* CPUID_H */
