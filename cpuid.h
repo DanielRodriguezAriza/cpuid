f6d399426aee3e804005809fff2fcc130b1a3ce7
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-11-16 20:21:11 +0100

    Added stdint support check

--- cpuid.h
+++ cpuid.h
@@ -1,14 +1,6 @@
 #ifndef CPUID_H
 #define CPUID_H
 
-#include <stdint.h>
-
-typedef union cpuid_t {
-	uint32_t a, b, c, d;
-	uint32_t eax, ebx, ecx, edx;
-	uint32_t regs[4];
-} cpuid_t;
-
 /* MSVC support */
 #if defined(_MSC_VER) && (defined(_WIN32) || defined(WIN32) || defined(_WIN64) || defined(WIN64))
 	#define CPUID_VERSION_MSVC
@@ -24,6 +16,25 @@ typedef union cpuid_t {
 #if defined(__clang__) || defined(__llvm__)
 	#define CPUID_VERSION_LLVM
 #endif
+
+/* Check for stdint support */
+#if defined(_MSC_VER) || (defined(__STDC_VERSION__) && __STDC_VERSION__ >= 199901L)
+	#include <stdint.h>
+	#define CPUID_U32_TYPE uint32_t
+#endif
+
+/* cpuid types */
+#ifndef CPUID_U32_TYPE
+	#define CPUID_U32_TYPE unsigned int
+#endif
+	
+typedef CPUID_U32_TYPE cpuid_u32_t;
+
+typedef union cpuid_t {
+	uint32_t a, b, c, d;
+	uint32_t eax, ebx, ecx, edx;
+	uint32_t regs[4];
+} cpuid_t;
 
 static inline void cpuid_call(uint32_t leaf, uint32_t subleaf, cpuid_t *registers)
 {
