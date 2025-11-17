cbae721147b2e76149ccd0b5745ea045ab17b30d
Daniel Rodríguez Ariza <136387964+DanielRodriguezAriza@users.noreply.github.com>
2025-11-17 17:33:57 +0100

    Implemented function to get CPU info
    This function returns the max value that the CPUID instruction can have on the EAX register (max leaf value), and sets the other registers (EBX, EDX and ECX) to the 12 char string value of the manufacturer id.

--- include/cpuid_util.h
+++ include/cpuid_util.h
@@ -12,6 +12,23 @@ CPUID_LINKAGE int cpuid_check(cpuid_u32_t leaf, cpuid_u32_t subleaf, cpuid_u32_t
 	return regs.regs[regidx] & (1 << bit);
 }
 
+// Returns the largest value that can be passed through EAX to the CPUID instruction
+// and sets the values of the registers EBX, EDX, ECX to a 12 char ASCII string with the manufacturer ID.
+int cpuid_check_info(cpuid_str_t *buf) CPUID_NOEXCEPT
+{
+	cpuid_reg_t x;
+	cpuid_call(0,0,&x);
+
+	buf->str[12] = 0;
+	// NOTE : Maybe rewrite this using memcpy instead of this UB ridden type punning... or use unions.
+	// All to avoid including extra headers like string.h lol...
+	*((cpuid_u32_t*)(buf->str) + 0) = x.ebx;
+	*((cpuid_u32_t*)(buf->str) + 1) = x.edx;
+	*((cpuid_u32_t*)(buf->str) + 2) = x.ecx;
+	
+	return x.eax;
+}
+
 int cpuid_check_sse         (void) CPUID_NOEXCEPT { return cpuid_check(1, 0, 3, 25); }
 int cpuid_check_sse2        (void) CPUID_NOEXCEPT { return cpuid_check(1, 0, 3, 26); }
 int cpuid_check_sse3        (void) CPUID_NOEXCEPT { return cpuid_check(1, 0, 2, 0); }
